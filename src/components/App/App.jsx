a45b7835a44910280363b917af9b4e82a9885115
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-06 14:50:33 +0100

    feat: add label input component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import ActivityCard from '../Reusable/ActivityCard/ActivityCard';
 import Footer from '../Reusable/Footer/Footer';
 import IconButton from '../Reusable/IconButton/IconButton';
+import LabelInput from '../Reusable/LabelInput/LabelInput';
 import SimpleButton from '../Reusable/SimpleButton/SimpleButton';
 import TravelersList from '../Reusable/TravelersList/TravelersList';
 import User from '../Reusable/User/User';
@@ -10,18 +11,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <p>Simple Button</p>
-      <SimpleButton textContent='Continuer'/>
-      <p>Icon Button</p>
-      <IconButton icon="trash" textContent='Supprimer'/>
-      <p>User</p>
-      <User firstName="Nicolas" lastName='Guillotte'/>
-      <p>Activity Card</p>
-      <ActivityCard />
-      <p>Travelers List</p>
-      <TravelersList />
-      <p>Footer</p>
-      <Footer />
+        <p>Simple Button</p>
+        <SimpleButton textContent="Continuer" />
+        <p>Icon Button</p>
+        <IconButton icon="trash" textContent="Supprimer" />
+        <p>User</p>
+        <User firstName="Nicolas" lastName="Guillotte" />
+        <p>Activity Card</p>
+        <ActivityCard />
+        <p>Travelers List</p>
+        <TravelersList />
+        <p>Label input</p>
+        <LabelInput label="Renseignez le coût moyen" placeholder="votre texte" value="" />
+        <LabelInput label="Test 2" placeholder="f" value="coucou" />
+        <LabelInput label="Test 3" placeholder="f" value="coucou" />
+        <p>Footer</p>
+        <Footer />
       </header>
     </div>
   );
