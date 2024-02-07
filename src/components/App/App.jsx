f71d044e700ff71dd735026234922f7810877615
MelodyPatin <melody.patin@oclock.school>
2024-02-07 17:17:01 +0100

    feat: add TravelCard Component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,12 @@
-import ActivityCard from '../Reusable/ActivityCard/ActivityCard';
-import Footer from '../Reusable/Footer/Footer';
-import IconButton from '../Reusable/IconButton/IconButton';
-import LabelInput from '../Reusable/LabelInput/LabelInput';
-import NavBarNico from '../Reusable/NavBarNico/NavBarNico';
-import SimpleButton from '../Reusable/SimpleButton/SimpleButton';
-import TravelersList from '../Reusable/TravelersList/TravelersList';
-import User from '../Reusable/User/User';
 import HomePage from '../HomePage/HomePage';
+import TravelCardCurrent from '../Reusable/TravelCard/TravelCard';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HomePage />
+        <TravelCardCurrent title="Coucou" />
       </header>
     </div>
   );
