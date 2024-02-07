15fd5c45f4b65f77e06af245abc8a6fd7f88a5e6
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-07 15:34:07 +0100

    feat: add popup input

--- src/components/Reusable/Popups/PopupInput.jsx
+++ src/components/Reusable/Popups/PopupInput.jsx
@@ -3,22 +3,28 @@ import './Popups.scss';
 import PropTypes from 'prop-types';
 import { XCircleIcon } from '@heroicons/react/24/solid';
 import SimpleButton from '../SimpleButton/SimpleButton';
+import LabelInput from '../LabelInput/LabelInput';
+import { Input } from 'semantic-ui-react';
 
-const PopupButton = ({ textContent, buttonContent }) => {
+const PopupInput = ({ textContent, buttonContent }) => {
   return (
     <div className="container">
       <div className="Popup">
         <XCircleIcon className="icon" />
         <p>{textContent}</p>
+        <Input placeholder="Prénom" />
+        <Input placeholder="Nom" />
+        <Input placeholder="Email" />
+        <Input placeholder="Mot de passe" />
         <SimpleButton textContent={buttonContent} />
       </div>
     </div>
   );
 };
 
-PopupButton.propTypes = {
+PopupInput.propTypes = {
   textContent: PropTypes.string.isRequired,
   buttonContent: PropTypes.string.isRequired,
 };
 
-export default PopupButton;
+export default PopupInput;
