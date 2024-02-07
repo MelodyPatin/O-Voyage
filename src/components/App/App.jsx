b895fd066f2a0b538e1abc2783e4f6b4e7eaad35
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-07 15:37:58 +0100

    feat: add popup input

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,11 @@
 import HomePage from '../HomePage/HomePage';
-import PopupButton from '../Reusable/Popups/PopupButton';
-import PopupInput from '../Reusable/Popups/PopupInput';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PopupInput textContent="Nouveau compte" buttonContent="S'inscrire" />
+        <HomePage />
       </header>
     </div>
   );
