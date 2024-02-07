2aca70f637c5ca2e5687271801191213ab9766f8
MelodyPatin <melody.patin@oclock.school>
2024-02-07 09:10:36 +0100

    feat : homepage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,30 +6,14 @@ import NavBarNico from '../Reusable/NavBarNico/NavBarNico';
 import SimpleButton from '../Reusable/SimpleButton/SimpleButton';
 import TravelersList from '../Reusable/TravelersList/TravelersList';
 import User from '../Reusable/User/User';
+import HomePage from '../HomePage/HomePage';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Navbar</p>
-        <NavBarNico />
-        <p>Simple Button</p>
-        <SimpleButton textContent="Continuer" />
-        <p>Icon Button</p>
-        <IconButton icon="trash" textContent="Supprimer" />
-        <p>User</p>
-        <User firstName="Nicolas" lastName="Guillotte" />
-        <p>Activity Card</p>
-        <ActivityCard />
-        <p>Travelers List</p>
-        <TravelersList />
-        <p>Label input</p>
-        <LabelInput label="Renseignez le coût moyen" placeholder="votre texte" value="" />
-        <LabelInput label="Test 2" placeholder="f" value="coucou" />
-        <LabelInput label="Test 3" placeholder="f" value="coucou" />
-        <p>Footer</p>
-        <Footer />
+        <HomePage />
       </header>
     </div>
   );
