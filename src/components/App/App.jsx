2fd5a4b7e7de2e0cbbf5d983696bcbfe4c0abe08
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-07 17:09:42 +0100

    fix: resolve conflict to merge

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,11 @@
-import ActivityCard from '../Reusable/ActivityCard/ActivityCard';
-import Footer from '../Reusable/Footer/Footer';
-import IconButton from '../Reusable/IconButton/IconButton';
-import SimpleButton from '../Reusable/SimpleButton/SimpleButton';
-import User from '../Reusable/User/User';
+import HomePage from '../HomePage/HomePage';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <p>Simple Button</p>
-      <SimpleButton textContent='Continuer'/>
-      <p>Icon Button</p>
-      <IconButton icon="trash" textContent='Supprimer'/>
-      <p>User</p>
-      <User firstName="Nicolas" lastName='Guillotte'/>
-      <p>Activity Card</p>
-      <ActivityCard activityTitle="Visite du Parlement de Budapest" />
-      <p>Footer</p>
-      <Footer />
+        <HomePage />
       </header>
     </div>
   );
