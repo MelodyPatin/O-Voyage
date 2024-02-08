8a052509e16dff263d1ef8e98b48ba381bc3909d
MelodyPatin <melody.patin@oclock.school>
2024-02-08 14:54:04 +0100

    fix : change App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,11 @@
-import Error from '../Pages/Error/Error';
+import HomePage from '../Pages/HomePage/HomePage';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Error
-          number="ERREUR 404"
-          textContent="Désolé, la page que vous recherchez n'existe pas. Veuillez vérifier l'URL et réessayer."
-        />
+        <HomePage />
       </header>
     </div>
   );
