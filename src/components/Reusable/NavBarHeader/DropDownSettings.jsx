0caabdf2a096b6d4753d4dd37790bf108f8c5120
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-08 14:59:47 +0100

    feat: navbar header ok

--- src/components/Reusable/NavBarHeader/DropDownSettings.jsx
+++ src/components/Reusable/NavBarHeader/DropDownSettings.jsx
@@ -1,23 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import {
-  DropdownMenu,
-  DropdownItem,
-  DropdownDivider,
-  Dropdown,
-} from 'semantic-ui-react';
+  ChevronUpIcon,
+  ChevronDownIcon,
+  BellIcon,
+  XCircleIcon,
+} from '@heroicons/react/24/solid';
+import User from '../User/User';
+import Avatar from '../Avatar/Avatar';
+import ProfileInfo from '../ProfileInfo/ProfileInfo';
 
-const DropDownSettings = ({ label }) => (
-  <Dropdown text={label} direction="left">
-    <DropdownMenu>
-      <DropdownItem text="Modifier mon profil" />
-      <DropdownItem text="Mes amis" />
-      <DropdownItem text="Ajouter un ami" />
-      <DropdownItem text="Se déconnecter" />
-    </DropdownMenu>
-  </Dropdown>
-);
-DropDownSettings.propTypes = {
-  label: PropTypes.string.isRequired,
+const DropDownSettings = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const onMobile = false;
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  console.log(isOpen);
+
+  return (
+    <div className="DropDownMenu">
+      <div className="dropdown-trigger" onClick={toggleMenu}>
+        <div className="text">
+          <Avatar className="avatar" />
+          <p className="notification-title">Vous</p>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="dropdown-content settings">
+          {/* Contenu du menu déroulant */}
+          {onMobile && <XCircleIcon className="cross-icon" />}
+          {!onMobile && <ProfileInfo
+            firstName={'Mélody'}
+            nbTravels={5}
+            nbFriends={10}
+            id="info"
+          />}
+          <ul>
+            <li className="item">Modifier mon profil</li>
+            <li className="item">Mes amis</li>
+            <li className="item">Ajouter un ami</li>
+            <li className="item">Se déconnecter</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 };
+
 export default DropDownSettings;
