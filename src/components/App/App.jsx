2b0fceffe8351a248cd08a318ad6343c63ebdc48
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-09 17:09:18 +0100

    feat: add update travel and activity on mobile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,12 @@
 import FriendAdd from '../Pages/Friend/FriendAdd/FriendAdd';
 import './App.scss';
-<<<<<<< HEAD
 import HomePage from '../Pages/HomePage/HomePage';
-=======
->>>>>>> develop
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <HomePage />
-=======
-        <FriendAdd />
->>>>>>> develop
       </header>
     </div>
   );
