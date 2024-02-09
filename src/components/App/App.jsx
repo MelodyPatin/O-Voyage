d525e008f4d4ce9cdc8ca40a472d4e9cd7c0a6ef
MelodyPatin <melody.patin@oclock.school>
2024-02-09 12:02:37 +0100

    feat : add header for connected version

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,12 @@
-import { Popup } from 'semantic-ui-react';
-import NavBarHeader from '../Reusable/NavBarHeader/NavBarHeader';
+import HomePage from '../Pages/HomePage/HomePage';
+import HeaderConnected from '../Reusable/HeaderConnected/HeaderConnected';
 import './App.scss';
-import PopupInput from '../Reusable/Popups/PopupInput';
-import TravelCard from '../Reusable/TravelCard/TravelCard';
-import TravelersList from '../Reusable/TravelersList/TravelersList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TravelersList />
+        <HeaderConnected />
       </header>
     </div>
   );
