504225e509cafe6f9d3dddee5ddbfa5f7bc84374
MelodyPatin <melody.patin@oclock.school>
2024-02-12 11:18:09 +0100

    feat: add page dashboard

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,14 @@
 import './App.scss';
 import TravelDetails from '../Pages/Travel/TravelDetails/TravelDetails';
 import ActivityResume from '../Pages/ActivityResume/ActivityResume';
+import UserUpdate from '../Pages/User/UserUpdate';
+import FriendList from '../Pages/Friend/FriendList/FriendList';
+import Dashboard from '../Pages/Dashboard/Dashboard';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TravelDetails onDesktop Activity />
+        <Dashboard />
       </header>
     </div>
   );
