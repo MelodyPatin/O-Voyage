8f2771d27d4694cbe9e3df56499bb2c97fbcd4b1
MelodyPatin <melody.patin@oclock.school>
2024-02-17 16:32:56 +0100

    feat : add user data in user form

--- src/components/Pages/User/FormUserUpdate.jsx
+++ src/components/Pages/User/FormUserUpdate.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './UserUpdate.scss';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
@@ -8,6 +9,10 @@ import SimpleButton from '../../Reusable/SimpleButton/SimpleButton';
 import ReturnTitle from '../../Reusable/ReturnTitle/ReturnTitle';
 
 const FormUserUpdate = ({ FirstName, LastName, Email }) => {
+  const user = useSelector((state) => state.user);
+
+  console.log(user);
+
   const navigate = useNavigate();
 
   const handleGoBack = (event) => {
@@ -20,9 +25,13 @@ const FormUserUpdate = ({ FirstName, LastName, Email }) => {
       <ReturnTitle textContent="Modifier mon profil" />
       <div className="userUpdate">
         <form className="FormUpdateProfile" action="">
-          <LabelInput label="Prénom" placeholder="" value={FirstName} />
-          <LabelInput label="Nom" placeholder="" value={LastName} />
-          <LabelInput label="Email" placeholder="" value={Email} />
+          <LabelInput
+            label="Prénom"
+            placeholder=""
+            value={user.firstnameValue}
+          />
+          <LabelInput label="Nom" placeholder="" value={user.lastnameValue} />
+          <LabelInput label="Email" placeholder="" value={user.email} />
           <LabelInput label="Mot de passe" placeholder="" value="" />
           <div className="LabelInput">
             <p>Photo de profil</p>
