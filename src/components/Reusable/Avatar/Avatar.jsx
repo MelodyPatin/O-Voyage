51961c13a033479e0b6f538c907933b1e5b29335
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-21 16:54:12 +0100

    feat: add border on avatar picture

--- src/components/Reusable/Avatar/Avatar.jsx
+++ src/components/Reusable/Avatar/Avatar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Image } from 'semantic-ui-react';
+import './Avatar.scss';
 
 // Avatar component that displays an image
 const Avatar = () => {
