26e7c809d09d160014f3ada7ff0f3c1d4e14410f
MelodyPatin <melody.patin@oclock.school>
2024-02-21 09:13:29 +0100

    feat : faq and our history fetch data problem

--- src/components/Pages/FAQ/FAQ.jsx
+++ src/components/Pages/FAQ/FAQ.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import NavBarHeader from '../../Reusable/NavBarHeader/NavBarHeader';
 import ReturnTitle from '../../Reusable/ReturnTitle/ReturnTitle';
 import Footer from '../../Reusable/Footer/Footer';
@@ -8,11 +8,6 @@ import ContentFAQ from './ContentFAQ';
 import { fetchFaqContent } from '../../../actions/content';
 
 const FAQ = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchFaqContent());
-  }, [dispatch]);
 
   return (
     <div className="faq">
