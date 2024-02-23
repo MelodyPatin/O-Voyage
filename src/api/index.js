8d5b1920ab126ce194ff38b8ad58d6b084041479
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-23 16:51:30 +0100

    test: api http address to test deployment

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8001/api',
+  baseURL: 'http://theophile-bernard-server.eddi.cloud/api',
 });
 // intercepteur de request avec axios
 api.interceptors.request.use(
