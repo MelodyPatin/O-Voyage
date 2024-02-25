248d239eaa3f8eb609c18ca321a89762e0952bf9
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-25 11:31:20 +0100

    feat: better css for multiple selector

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://theophile-bernard-server.eddi.cloud/api/api',
+  baseURL: 'http://localhost:8001/api',
 });
 // intercepteur de request avec axios
 api.interceptors.request.use(
