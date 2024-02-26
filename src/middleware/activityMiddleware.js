310de92cc4eebe789545821f7673722cfde9c122
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-26 11:56:32 +0100

    mvp ok

--- src/middleware/activityMiddleware.js
+++ src/middleware/activityMiddleware.js
@@ -40,6 +40,7 @@ const activityMiddleware = (store) => (next) => async (action) => {
       api
         .get(`/trip/${action.tripId}/activities`)
         .then((response) => {
+          console.log(response.data);
           store.dispatch(saveTripActivities(response.data));
         })
         .catch((error) => {
