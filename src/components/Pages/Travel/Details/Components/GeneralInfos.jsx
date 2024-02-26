9f53b61324d374bf231ca7265b6285cbd4e3acea
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-26 14:46:26 +0100

    feat: add description and cities to general info of a travel

--- src/components/Pages/Travel/Details/Components/GeneralInfos.jsx
+++ src/components/Pages/Travel/Details/Components/GeneralInfos.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import TravelPicture from '../../../../Reusable/TravelPicture/TravelPicture';
 import TravelDates from './TravelDates';
 import './GeneralInfos.scss';
 
 const GeneralInfos = () => {
+  const description = useSelector((state) => state.trip.trip.description);
+  const cities = useSelector((state) => state.trip.trip.cities);
+
+  // Map over the cities array to extract the names
+  const cityNames = cities.map(city => city.name);
+
+  // Join the city names into a single string separated by commas
+  const cityNamesString = cityNames.join(', ');
+
   return (
     <div>
       <TravelPicture />
       <TravelDates />
+      <div className='description'>{description}</div>
+      <div className='city-names'>{cityNamesString}</div>
     </div>
   );
 };
