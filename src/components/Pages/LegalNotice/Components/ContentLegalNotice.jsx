a8893ceecedc684efd2dc68add66fb668947a47e
nicolas-guillotte <nicolas.guillotte@oclock.school>
2024-02-27 16:59:54 +0100

    minor fixes

--- src/components/Pages/LegalNotice/Components/ContentLegalNotice.jsx
+++ src/components/Pages/LegalNotice/Components/ContentLegalNotice.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 
 const ContentLegalNotice = () => {
@@ -7,86 +6,34 @@ const ContentLegalNotice = () => {
       <li>
         <p className="question">1. Éditeur du Site :</p>
         <p className="answer">
-          [Nom de votre entreprise] <br />
-          [Adresse de l'entreprise] <br /> [Numéro de téléphone de l'entreprise]{' '}
-          <br /> [Adresse e-mail de l'entreprise]
+          Ovoyage <br />
+          5 lotissement le Clos de Goulien 29160 Crozon <br />
+          melody.patin.dev@gmail.com
         </p>
       </li>
       <li>
-        <p className="question">2. Directeur de la Publication :</p>
+        <p className="question">2. Hébergeur du Site :</p>
         <p className="answer">
-          [Nom du directeur de la publication] <br />
-          [Adresse e-mail du directeur de la publication]
+          OVH <br />
+          2 RUE KELLERMANN 59100 ROUBAIX
         </p>
       </li>
       <li>
-        <p className="question">3. Hébergeur du Site : </p>
+        <p className="question">3. Accès au Site :</p>
         <p className="answer">
-          [Nom de l'hébergeur]
-          <br /> [Adresse de l'hébergeur] <br />
-          [Numéro de téléphone de l'hébergeur] <br />
-          [Adresse e-mail de l'hébergeur]
+          Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance. En cas de modification, interruption ou suspension du Site, l’Editeur ne saurait être tenu responsable.
         </p>
       </li>
       <li>
-        <p className="question">4. Propriété Intellectuelle : </p>
+        <p className="question">4. Collecte des Données :</p>
         <p className="answer">
-          Tous les contenus présents sur le site, tels que les textes, images,
-          logos, et autres éléments, sont la propriété exclusive de [Nom de
-          votre entreprise]. Toute utilisation non autorisée de ces contenus est
-          interdite.
+          Le Site assure à l’Utilisateur une collecte et un traitement d’informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés. En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l’Utilisateur dispose d’un droit d’accès, de rectification, de suppression et d’opposition de ses données personnelles. L’Utilisateur exerce ce droit : par mail à l’adresse email melody.patin.dev@gmail.com ou via son espace personnel.
         </p>
       </li>
       <li>
-        <p className="question">5. Données Personnelles : </p>
+        <p className="question">5. Propriété Intellectuelle :</p>
         <p className="answer">
-          [Nom de votre entreprise] s'engage à protéger la vie privée de ses
-          utilisateurs. Les données personnelles collectées sont traitées
-          conformément à notre politique de confidentialité, disponible à [lien
-          vers la politique de confidentialité].
-        </p>
-      </li>
-      <li>
-        <p className="question">6. Cookies : </p>
-        <p className="answer">
-          Le site utilise des cookies pour améliorer l'expérience utilisateur.
-          En naviguant sur le site, vous acceptez l'utilisation de ces cookies.
-          Vous pouvez en savoir plus sur notre utilisation des cookies dans
-          notre politique de confidentialité.
-        </p>
-      </li>
-      <li>
-        <p className="question">7. Responsabilité : </p>
-        <p className="answer">
-          [Nom de votre entreprise] décline toute responsabilité quant à
-          l'utilisation du site et des informations qui y sont contenues. Les
-          utilisateurs sont responsables de l'exactitude et de la légalité des
-          informations qu'ils partagent et des activités qu'ils organisent via
-          la plateforme.
-        </p>
-      </li>
-      <li>
-        <p className="question">8. Liens Externes : </p>
-        <p className="answer">
-          Le site peut contenir des liens vers des sites tiers. [Nom de votre
-          entreprise] n'assume aucune responsabilité quant au contenu de ces
-          sites externes.
-        </p>
-      </li>
-      <li>
-        <p className="question">9. Droit Applicable et Juridiction : </p>
-        <p className="answer">
-          Les présentes mentions légales sont régies par le droit applicable en
-          [lieu de juridiction]. En cas de litige, les tribunaux de [lieu de
-          juridiction] seront seuls compétents.
-        </p>
-      </li>
-      <li>
-        <p className="question">10. Contact : </p>
-        <p className="answer">
-          Pour toute question ou demande concernant les mentions légales,
-          veuillez nous contacter à l'adresse suivante : [Adresse e-mail de
-          contact].
+          Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site, sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
         </p>
       </li>
     </ul>
